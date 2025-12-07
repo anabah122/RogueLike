1972d1a2b3096b487a67ec8d96b7011ed1c6bbd3
anabah122 <roflercast@mail.ru>
2025-12-07 03:12:18 +0300

    add new mods

--- DATA/eventsList.lua
+++ DATA/eventsList.lua
@@ -87,11 +87,85 @@ eventsList['tunderShield'] =
         
         func = function( self, eventName, args ) 
             local player, enemy = unpack( args )
-            player:CastSpell( enemy, self.id , true )
+            local group = player:GetGroup()
+            group = group or { player }
+
+            for _, p in ipairs( group ) do 
+                player:CastSpell( p, self.id , true )
+            end
         end ,
         
         triggers = { -- bool 
-            onLoggin=true
+            onLoggin=true, onCombatStart=true
+        }
+    })
+
+    
+eventsList['shadowShield'] = 
+    eventObj:new({
+        id=22417 , 
+        desc='Щит Тени|при входе в бой на 30 сек вы получаете щит который поглощает до 400 урона' , 
+        icon='spell_shadow_antishadow',
+        
+        func = function( self, eventName, args ) 
+            local player, enemy = unpack( args )
+            local group = player:GetGroup()
+            group = group or { player }
+
+            for _, p in ipairs( group ) do 
+                player:AddAura( self.id , p )
+            end
+        end ,
+        
+        triggers = { -- bool 
+            onLoggin=true, onCombatStart=true
+        }
+    })
+
+
+eventsList['wildHeal'] = 
+    eventObj:new({
+        id=9616 , 
+        desc='Дикое востановление|при победой над противником c 20% шансом восполняет члену группы 23-37 здоровья раз в 2 сек. в течение 20 сек' , 
+        icon='spell_nature_rejuvenation',
+        
+        func = function( self, eventName, args ) 
+            local player, enemy = unpack( args )
+            local group = player:GetGroup()
+            group = group or { player }
+
+            for _, p in ipairs( group ) do 
+                if math.random(10)<2 then 
+                    player:AddAura( self.id , p )
+                end
+            end
+        end ,
+        
+        triggers = { -- bool 
+            onKillCreature=true
+        }
+    })
+
+eventsList['manaTitan'] = 
+    eventObj:new({
+        id=50983 , 
+        desc='Титан магии|при исцелении и нанесения урона магическими умениями с шансом 2% члены группы могут получить эффект|восполнение 10% маны в секунду в течение 10 сек, получаемый урон повышается на 400%.' , 
+        icon='ability_mage_shattershield',
+        
+        func = function( self, eventName, args ) 
+            local player, enemy = unpack( args )
+            local group = player:GetGroup()
+            group = group or { player }
+
+            for _, p in ipairs( group ) do 
+                if math.random(100)<2 and p:HasCasterSpec() then 
+                    player:AddAura( self.id , p )
+                end
+            end
+        end ,
+        
+        triggers = { -- bool 
+            onSpellCast=true
         }
     })
 
