eb1f06f1bd58cf89f87d5a7f5efdfd21c56b8539
anabah122 <roflercast@mail.ru>
2025-12-08 23:40:37 +0300

    final gossip system and ev data

--- DATA/eventLoader.lua
+++ DATA/eventLoader.lua
@@ -1,16 +1,16 @@
 
 
-function regGossipEv( npcId , name )
+function regGossipEv( npcId , evObj )
 
     local f1 = RegisterCreatureGossipEvent(npcId, 1, function(event, player, creature)
-        player:GossipMenuAddItem(0, name, 0, 128)
+        player:GossipMenuAddItem(0, evObj.name, 0, evObj.id)
         player:GossipSendMenu(1, creature)
     end)
 
     local f2 = RegisterCreatureGossipEvent(npcId, 2, function(event, player, creature, sender, intid, code, menu_id)
-        if intid == 128 then
-            player:SendBroadcastMessage("Вы получили улучшение "..name)
-            eventObj:register( player )
+        if intid == evObj.id then
+            player:SendBroadcastMessage("Вы получили улучшение "..evObj.name)
+            evObj:register( player )
             player:GossipComplete()
         end
     end)
